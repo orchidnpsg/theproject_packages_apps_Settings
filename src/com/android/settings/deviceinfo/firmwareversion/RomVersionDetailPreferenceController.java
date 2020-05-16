00f107e84c3e1b470b43116d0e462e7d7d8e04af
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-05-16 23:22:25 +0530

    settings: fix derp

--- src/com/android/settings/deviceinfo/firmwareversion/RomVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/RomVersionDetailPreferenceController.java
@@ -34,8 +34,8 @@ public class RomVersionDetailPreferenceController extends BasePreferenceControll
 
     private static final Uri INTENT_URI_DATA = Uri.parse("https://paypal.me/");
     private static final String TAG = "romDialogCtrl";
-    private static final String KEY_ROM_VERSION_PROP = "org.atom.build_version";
-    private static final String KEY_ROM_RELEASETYPE_PROP = "org.atom.build_type";
+    private static final String KEY_ROM_VERSION_PROP = "org.fusion.build_version";
+    private static final String KEY_ROM_RELEASETYPE_PROP = "org.fusion.build_type";
     private final PackageManager mPackageManager = this.mContext.getPackageManager();
 
     public RomVersionDetailPreferenceController(Context context, String preferenceKey) {
